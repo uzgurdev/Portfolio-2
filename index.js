bc2b9bc951d504e4a599f215361e5f598cf325fc
Пользователь <uzbmv2@gmail.com>
2022-09-20 18:05:52 +0500

    Saytga js qo'shilyapti va yangi qisim ham

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function bosil(r) {
+
+    let li = document.querySelectorAll(".links");
+
+    let home = document.getElementById("home");
+
+    if (home == 0) {
+        r.classList.add("active");
+    } else {
+
+        for (let i = 0; i < btn.length; i++) {
+
+            btn[i].classList.remove('active');
+
+        }
+
+    }
+
+}
