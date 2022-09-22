3f784af944e556c4f4ed972bac4360b6b6213a8d
Пользователь <uzbmv2@gmail.com>
2022-09-22 17:54:08 +0500

    Ixtiyor oka manashu ko'dlari

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 
-function bosil(r) {
+$(documnt).ready(function () {
+    $('.all').click(function () {
+        $(".images-img").css("display: none;")
+    })
+})
 
-    let li = document.querySelectorAll(".links");
 
-    let home = document.getElementById("home");
-
-    if (home == 0) {
-        r.classList.add("active");
-    } else {
-
-        for (let i = 0; i < btn.length; i++) {
-
-            btn[i].classList.remove('active');
-
-        }
-
-    }
-
-}
