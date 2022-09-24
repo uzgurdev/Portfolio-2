9e7d46b9b2215ac6a4ee6a35dd0cd95f5cba8a8c
Пользователь <uzbmv2@gmail.com>
2022-09-24 12:59:14 +0500

    saytni filter qismi ishladi faqat to'liq emas

--- index.js
+++ index.js
@@ -1,25 +1,222 @@
 
-$(document).ready(function(){
-   
-    $(".all").click(function(){
-        
+$(document).ready(function () {
+
+    $(".all").click(function () {
+
         $("#userinter").css({
 
-          "display":"none",
-          
+            "display": "block",
+
         })
-        
-       
+
+        $("#userinter2").css({
+
+            "display": "block",
+
+        })
+
+        $("#printtempl").css({
+
+            "display": "block",
+
+        })
+
+        $("#printtempl2").css({
+
+            "display": "block",
+
+        })
+
+        $("#mockup").css({
+
+            "display": "block",
+
+        })
+
+        $("#mockup2").css({
+
+            "display": "block",
+
+        })
+
+        $("#webtempl").css({
+
+            "display": "block",
+
+        })
+
+        $("#webtempl2").css({
+
+            "display": "block",
+
+        })
+
+
     })
-    $(".all1").click(function(){
-        
+
+    $(".prt").click(function () {
+
         $("#userinter").css({
 
-          "display":"block",
-          
+            "display": "none",
+
         })
-        
-       
+
+        $("#userinter2").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup2").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl2").css({
+
+            "display": "none",
+
+        })
+
+
+    })
+
+    $(".wbt").click(function () {
+
+        $("#userinter").css({
+
+            "display": "none",
+
+        })
+
+        $("#userinter2").css({
+
+            "display": "none",
+
+        })
+
+        $("#printtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#printtempl2").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup2").css({
+
+            "display": "none",
+
+        })
+
+    })
+
+    $(".uri").click(function () {
+
+        $("#printtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#printtempl2").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup").css({
+
+            "display": "none",
+
+        })
+
+        $("#mockup2").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl2").css({
+
+            "display": "none",
+
+        })
+
+
+    })
+
+    $(".mkp").click(function () {
+
+        $("#userinter").css({
+
+            "display": "none",
+
+        })
+
+        $("#userinter2").css({
+
+            "display": "none",
+
+        })
+
+        $("#printtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#printtempl2").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl").css({
+
+            "display": "none",
+
+        })
+
+        $("#webtempl2").css({
+
+            "display": "none",
+
+        })
+
+        console.log(0);
+
+
     })
 
 })
