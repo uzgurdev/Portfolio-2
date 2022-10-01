f883426491c3634129cbe937bd78436da062286e
Пользователь <uzbmv2@gmail.com>
2022-10-01 23:12:50 +0500

    team bo'limiga JS qo'shilyapti

--- index.js
+++ index.js
@@ -224,14 +224,16 @@ function pres(imagess) {
 
     mainimg.src = imagess.src;
 
-    let h3 = document.getElementsByTagName('h3');
+    let membernames = document.getElementsByTagName('h3');
 
-    for (let i = 0; i < h3.length; i++) {
+    // for (let i = 0; i < h3.length; i++) {
 
-        h3[i].style = " ";
+    //     h3[i].style = " ";
 
-    }
+    // }
 
-    h3.style = "top: 177px;width: 197px;height: 37px;color: #fff;line - height: 2.2;position: absolute;text - align: center;font - family: sans - serif;transition: all 5s;transform: translateY(400px);background: rgba(0, 0, 0, 0.7); "
+    // h3.style = "top: 177px;width: 197px;height: 37px;color: #fff;line - height: 2.2;position: absolute;text - align: center;font - family: sans - serif;transition: all 5s;transform: translateY(400px);background: rgba(0, 0, 0, 0.7); ";
+
+    membernames.classList.add("active-member")
 
 }
